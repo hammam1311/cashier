47ee708c3fff06597b8ae83c7262dd3985de6de7
hammam <h.besaiso95@gmail.com>
2020-02-06 15:29:48 +0200

    now you can run you super market

--- cashier.py
+++ cashier.py
@@ -1,5 +1,24 @@
 def main():
 	# write code here
+	list=[]
+	while True :
+		item = input("item (enter -done- when finished): ")
+		if item == "done":
+			break
+		price = input("price : ")
+		q = input("quantity : ")
+		list.append({"item":item , "price":price ,"q":q})
+		continue
+	total=0
+	for x in list :
+
+		i1 = x["item"]
+		q1 = x["q"]
+		p1 = x["price"]
+		print (str(q1) +" "+ str(i1) +" "+ str(int(p1)*int(q1))+" JD ")
+		total += int(p1)*int(q1)
+		
+	print ("Total Price: "+ str(total))
 
 if __name__ == '__main__':
 	main()
